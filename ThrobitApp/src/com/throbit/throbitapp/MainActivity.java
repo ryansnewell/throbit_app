e72305d5ee2c1ebdd7da0c44ff51ae31d44eb715
thekung1324 <thekingdoof1324@gmail.com>
2014-01-25 07:37:32 -0600

    Building UI

--- ThrobitApp/src/com/throbit/throbitapp/MainActivity.java
+++ ThrobitApp/src/com/throbit/throbitapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.throbit.throbitapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
@@ -11,7 +13,9 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		ListView listView = (ListView)this.findViewById(R.id.postListView);
 		
+		ArrayAdapter<String> itemAdapter =  new ArrayAdapter<String>(this, , textViewResourceId)
 	}
 
 	@Override
