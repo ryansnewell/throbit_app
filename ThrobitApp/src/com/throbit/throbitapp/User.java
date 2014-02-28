e7e118283366236cfacac16bbb4d457394507e20
thekung1324 <thekingdoof1324@gmail.com>
2014-02-28 15:12:06 -0600

    Created User object

---
+++ ThrobitApp/src/com/throbit/throbitapp/User.java
@@ -0,0 +1,12 @@
+package com.throbit.throbitapp;
+
+public class User(){
+    
+    public static String username;
+    public static byte[] passwd;
+    
+    public static String getUsername(){ return username;}
+    public static byte[] getPasswdHash(){ return passwd;}
+    
+    
+}
